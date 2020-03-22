faa2eeffb25f1e1bb9d8319ecd7d55d801e817ac
sai.ka <saika@zeta.tech>
2020-03-22 09:11:47 +0530

    Draw a single circle

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-var canvas = document.getElementById('myCanvas');
-console.log(canvas);
+var ctx = initalize_context();
+simulate(ctx);
+
+function simulate(ctx) { // draw
+    draw_circle(ctx, 100, 100, 5, '#000000');
+}
+
+function initalize_context() {
+    var canvas = document.getElementById('myCanvas');
+    return canvas.getContext("2d");
+}
+
+function draw_circle(ctx, x, y, r, colour) {
+    ctx.fillStyle = colour;
+    ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = '#000000';
+}
